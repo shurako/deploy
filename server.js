fdc7fd47d8e7f8d3782df3f5162c6b5ec0307694
DESKTOP-NCUSJM3\patry <patrykmendel8@gmail.com>
2021-10-01 09:51:19 +0200

    deploy 1.04

--- server.js
+++ server.js
@@ -18,6 +18,29 @@ app.use('/api/posts', posts)
 const port = process.env.PORT || 3001
 const uri = 'mongodb+srv://user1:12345@cluster0.vnpml.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'gym/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'gym/build', 'index.html'));
+    });
+  }
+  
+  // ** MIDDLEWARE ** //
+  const whitelist = ['http://localhost:3001', 'http://localhost:8080']
+  const corsOptions = {
+    origin: function (origin, callback) {
+      console.log("** Origin of request " + origin)
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        console.log("Origin acceptable")
+        callback(null, true)
+      } else {
+        console.log("Origin rejected")
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  }
 
 
  
